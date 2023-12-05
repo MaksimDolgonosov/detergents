e25d019243bfee77257097369c3ed15ccf851c48
github-MaksimDolgonosov <max_air@bk.ru>
2023-12-05 16:55:06 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <span className='logo'>Бытовая химия</span>
+            </div>
+            <div className='presentation'></div>
+        </header>
+    );
+}
+
+export default Header;
