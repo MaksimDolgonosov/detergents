23ecf02fa96c929e7500fc6520ffc3ed8bfdcc37
MAKSIM <max_air@bk.ru>
2023-12-07 19:57:08 +0300

    Navigation menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import './header.scss'
 
 import Logo from '../../images/logo/logo2.svg'
+import Enter from '../../images/icons/enter.png'
 const Header = () => {
     return (
         <header className='header'>
             <div className='container'>
-                <img src={Logo} className='logo' alt='logo'/>
+                <img src={Logo} className='logo' alt='logo' />
+                <nav className='header__nav'>
+                    <ul className='header__nav-items'>
+                        <li className='header__nav-item'><a href="sdf">О нас</a> </li>
+                        <li className='header__nav-item'><a href="sdf">Доставка</a></li>
+                        <li className='header__nav-item'><a href="sdf"><img src={Enter} alt='enter_logo'></img> Войти </a></li>
+                    </ul>
+                </nav>
             </div>
+
             <div className='presentation'></div>
         </header>
     );
