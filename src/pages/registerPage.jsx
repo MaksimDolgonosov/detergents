0c9b763fa4d177b6749b40ba06ffe5dfa5f26e05
MAKSIM <max_air@bk.ru>
2024-02-06 16:56:46 +0300

    Firebase+registerpage

--- src/pages/registerPage.jsx
+++ src/pages/registerPage.jsx
@@ -1,7 +1,38 @@
-export const RegisterPage = ()=>{
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import '../css/registerPage.scss';
+
+export const RegisterPage = () => {
+const [name, setName] = useState("");
+const [email, setEmail]= useState("");
+const [password, setPassword]= useState("");
+
+
+
     return (
-        <div>
-            Register
+        <div className="register_page">
+            <h2>Зарегистрируйтесь</h2>
+            <h3>Что бы продолжить</h3>
+            <form>
+                <label>
+                    <div className="register__name">Ваше имя</div>
+                    <input type="text" value={name} onChange={setName}/>
+                </label>
+                <label>
+                    <div className="register__login">Ваш email</div>
+                    <input type="text" placeholder='Ваш email' value={email} onChange={setEmail}/>
+                </label>
+                <label>
+                    <div className="register__password">Пароль</div>
+                    <input type="password" value={password} onChange={setPassword}/>
+                </label>
+                <div>
+                    <button type="submit">Зарегистрироваться</button>
+                </div>
+            </form>
+            <div className="login_redirect">Уже зарегистрированы? <Link to="/login">Войти</Link></div>
         </div>
+
     )
+
 }
