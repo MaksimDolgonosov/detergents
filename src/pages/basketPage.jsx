9581b7b3fbaf7ce673d0c62ccf9c6bdea13e9752
MAKSIM <max_air@bk.ru>
2024-02-20 16:55:10 +0300

    basket List

--- src/pages/basketPage.jsx
+++ src/pages/basketPage.jsx
@@ -1,7 +1,39 @@
+import '../css/basketPage.scss';
 
+import { useSelector } from "react-redux"
+import { MdDelete } from "react-icons/md";
 
 export function BasketPage() {
+
+  const { basket } = useSelector(state => state.user);
+
+
+  const basketList = basket.map(item => {
+    return (
+
+      <li className="basket__item" key={item.id}>
+        <span >{item.item}</span>
+        <button className="basket__btn">-</button>
+        <span className='basket__quantity'>{item.quantity}</span>
+        <button className="basket__btn">+</button>
+        <MdDelete />
+      </li>
+
+    )
+  })
+
+
+
   return (
-    <div>basketPage</div>
+    <div className="container">
+      <div className="basket">
+        <h3>Корзина</h3>
+        <ul>
+          {basketList}
+        </ul>
+      </div>
+    </div>
+
+
   )
 }
